eac546a0d467f77fb9f5c0b1f159c3c19e60a73c
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2025-12-16 09:47:12 +0700

    add global middleware

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,21 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import { isTest } from '../env.ts'
 
 const app = express()
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(
+  morgan('dev', {
+    skip: () => isTest(),
+  })
+)
 
 app.get('/health', (req, res) => {
   res.json({ message: 'hello' }).status(200)

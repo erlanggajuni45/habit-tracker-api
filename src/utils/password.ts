0ec7055878d61c49b0cd0306a1aaddf63fb8f40e
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-01-09 11:40:53 +0700

    rename password hasing function in password util

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt'
 import env from '../../env.ts'
 
-export const hashedPasswordd = async (password: string) => {
+export const hashPassword = async (password: string) => {
   return bcrypt.hash(password, env.BCRYPT_ROUNDS)
 }

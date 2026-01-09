882989a8487dbfb1ee0e52228284c20f7efdb8ba
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-01-09 11:28:21 +0700

    add generate JWT token to jwt util

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,5 +1,20 @@
+import { createSecretKey } from 'crypto'
+import env from '../../env.ts'
+import { SignJWT, type JWTPayload } from 'jose'
+
 export interface JwtPayload {
   id: string
   email: string
   username: string
+}
+
+export const generateToken = (payload: JwtPayload): Promise<string> => {
+  const secret = env.JWT_SECRET
+  const secretKey = createSecretKey(secret, 'utf-8')
+
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime(env.JWT_EXPIRES_IN || '7d')
+    .sign(secretKey)
 }

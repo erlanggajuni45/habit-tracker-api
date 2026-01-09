05420b25b11f149f61177c3d0748832d0209d671
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-01-09 11:41:54 +0700

    implement user registration logic

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,37 @@
 import type { Request, Response } from 'express'
+import { users, type NewUser } from '../db/schema/users.ts'
+import { hashPassword } from '../utils/password.ts'
+import db from '../db/connection.ts'
+import { generateToken } from '../utils/jwt.ts'
 
-export const register = (req: Request, res: Response) => {
+export const register = async (
+  req: Request<any, any, NewUser>,
+  res: Response
+) => {
   try {
+    const hashedPassword = await hashPassword(req.body.password)
+    const [user] = await db
+      .insert(users)
+      .values({
+        ...req.body,
+        password: hashedPassword,
+      })
+      .returning({
+        id: users.id,
+        email: users.email,
+        username: users.username,
+        firstName: users.firstName,
+        lastName: users.lastName,
+        createdAt: users.createdAt,
+      })
+
+    const token = await generateToken({
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    })
+
+    res.status(201).json({ message: 'User created successfully', user, token })
   } catch (e) {
     console.error('Registration error:', e)
     res.status(500).json({ error: 'Failed to create user' })
